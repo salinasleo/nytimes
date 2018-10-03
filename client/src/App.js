1a8aa677bbf267b76fdf1e3e60ed255bab218ee9
salinasleo <salinasleo@gmail.com>
2018-10-02 20:19:37 -0400

    seeded

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Books from "./pages/articles";
+// import Nav from "./components/Nav";
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="./img/nytimes_logo.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">The New York Times Article Saver</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Not Fake News.
         </p>
+        {/* <Nav />
+        <Books /> */}
       </div>
     );
   }
 }
 
+
 export default App;
+
